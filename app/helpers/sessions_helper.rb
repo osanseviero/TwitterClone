d01a692c49c79328102c42a00946e5dec9de6f2b
Omar S <osanseviero@gmail.com>
2015-07-02 22:43:13 -0500

    Added dropdown bar when you log in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,16 @@
 module SessionsHelper
+	# Logs in the given user
 	def log_in(user)
 		session[:user_id] = user.id 
 	end
 
+	# Checks if the user exists and returns it
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# Returns true if the user has logged in
+	def logged_in?
+		!current_user.nil?
 	end
 end
