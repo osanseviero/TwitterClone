662245e4fce6671ade3e36bb4b24bd4cf1502f6f
Omar S <osanseviero@gmail.com>
2015-07-08 12:06:01 -0500

    Redirects to the page you want after logging in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -59,4 +59,22 @@ module SessionsHelper
   def current_user?(user)
   	user == current_user
   end
+
+  # Redirects to stored location
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
+
+
+
+
+
+
 end
